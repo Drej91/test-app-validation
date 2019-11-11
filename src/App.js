efb7f9f4eceef1411432f5f6c2d4fe88cc5d32a4
unknown <andrejdjokic@ymail.com>
2019-11-11 16:23:02 +0100

    adding form and submiting data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Form from './Components/Form/form.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <div className="wrapper">
+          <Form/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Form/Form.component.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+
+import './form.style.css';
+
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            email: ''
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    handleChange = e => {
+        e.preventDefault();
+        const { name, value } = e.target;
+
+        this.setState({
+            [name]: value
+        })
+
+    }
+
+    render() {
+        return (
+        <div className="form-wrapper">
+          <h1>Registration</h1>
+          <form onSubmit={this.handleSubmit}>
+              <div className="username">
+                  <label htmlFor="username">Username</label>
+                  <input 
+                    type="text" 
+                    className="" 
+                    placeholder="Username..."
+                    name="username"
+                    onChange={this.handleChange}
+                        />
+              </div>
+
+              <div className="email">
+                  <label htmlFor="email">Email</label>
+                  <input 
+                    type="email" 
+                    className="" 
+                    placeholder="Email..."
+                    name="email"
+                    onChange={this.handleChange}
+                        />
+              </div>
+
+              <div className="password">
+                  <label htmlFor="password">Password</label>
+                  <input 
+                    type="password" 
+                    className="" 
+                    placeholder="Password..."
+                    name="password"
+                    onChange={this.handleChange}
+                        />
+              </div>
+              <div className="submitBtn">
+                    <button type="submit">Register In</button>
+                    <small>Already Have an Account?</small>
+              </div>
+          </form>
+      </div>
+        );
+    }
+}
+
+
+export default Form;
